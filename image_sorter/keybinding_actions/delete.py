3e5291cdb575ad5b3b474d073e56ce8a9c18d395
Yevhen Martynenko <yevhen.martynenko.v@gmail.com>
2025-03-02 09:49:42 +0200

    feat: add file removal with safe delete option

--- image_sorter/keybinding_actions/delete.py
+++ image_sorter/keybinding_actions/delete.py
@@ -1,5 +1,25 @@
 from pathlib import Path
 
 
-def delete_file(file_path: Path, safe: bool = False) -> None:
-    ...  # TODO: delete file
+def delete_file(file_path: Path, safe: bool = True) -> tuple[str, str]:
+    file_name: str = file_path.name
+
+    if safe:
+        try:
+            trash_dir: Path = Path("~/.trash/image_sorter/").expanduser()
+            Path(trash_dir).mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            return (f"Creating trash directory \"{trash_dir}\": {e}", "error")
+
+        try:
+            new_file_path: Path = Path(trash_dir) / file_name
+            file_path.rename(new_file_path)
+            return (f"File \"{file_name}\" successfully moved to \"{trash_dir}\"", "success")
+        except Exception as e:
+            return (f"Moving file \"{file_name}\" to trash: {e}", "error")
+    else:
+        try:
+            file_path.unlink()
+            return (f"File \"{file_name}\" permanently deleted from {file_path}", "success")
+        except Exception as e:
+            return (f"Permanently deleting file \"{file_name}\": {e}", "error")
