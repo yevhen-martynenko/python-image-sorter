faf73d204d05fc1b87cec53332a3f22d7eda875e
Yevhen Martynenko <yevhen.martynenko.v@gmail.com>
2025-03-02 09:51:33 +0200

    feat: add system default app support for opening images

--- image_sorter/keybinding_actions/open.py
+++ image_sorter/keybinding_actions/open.py
@@ -1,5 +1,13 @@
+import subprocess
+import sys
 from pathlib import Path
 
 
-def open_with_system_app():
-    ...
+def open_with_system_app(file_path: Path) -> None:
+    """Open an image with the default application"""
+    if sys.platform == "linux" or sys.platform == "linux2":
+        subprocess.run(["xdg-open", file_path])
+    elif sys.platform == "darwin":
+        subprocess.run(["open", file_path])  # For macOS
+    else:
+        print(f"Unsupported OS: {sys.platform}")
